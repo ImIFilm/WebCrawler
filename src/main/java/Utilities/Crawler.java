8fd3aceac5e2a9dc993af5b0b5fd25efd8987d7f
traveler <patryktraveler@gmail.com>
2018-12-08 21:54:09 +0100

    Minor Crawler update to make it fit new query format

--- src/main/java/Utilities/Crawler.java
+++ src/main/java/Utilities/Crawler.java
@@ -19,25 +19,15 @@ public class Crawler{
 
     public void startCrawling() {
         for(Query query: queries){
-            System.out.println(query.getUrl());
-            RegexpCreator regexp = new RegexpCreator(query.getSentencePattern(),query.getForbiddenWords());
-            if(!regexp.getValid()){
-                continue;
-            }
-            System.out.println(regexp.getSearchExpr());
-            query.setRegexp(regexp.getSearchExpr());
-
             HtmlParser htmlParser = new HtmlParser(query.getUrl());
             Elements downloadedWebsite;
             downloadedWebsite=htmlParser.parseToText();
             TextParser textParser = new TextParser(downloadedWebsite);
             List<String> allSentences = null;
             allSentences =textParser.makeSentences();
-            SearchPattern pattern = new SearchPattern(query.getRegexp());
-
 
             for(String sentence: allSentences){
-                if (pattern.matches(sentence)){
+                if (query.matches(sentence)){
                     urlPerSentences.add(new UrlPerSentence(query.getUrl(),sentence));
                 }
 
